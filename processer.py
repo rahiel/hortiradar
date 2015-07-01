ed7c266950159e56b56d1508c1f208cc85391c7e
Marijn ten Thij <mctenthij@gmail.com>
2015-07-01 14:26:08 +0200

    First commit: background application with simple tokenCount

---
+++ processer.py
@@ -0,0 +1,163 @@
+#!/usr/bin/python
+from tokencounts import tokenCounts
+from tweepy import StreamListener
+from tweepy.parsers import RawParser
+from twokenize import tokenizeRawTweetText
+import json, time, tweepy, sys, datetime, calendar, os, Queue, threading, ConfigParser
+
+Param = ConfigParser.ConfigParser()
+Param.read('parameters.ini')
+Twitdata = ConfigParser.ConfigParser()
+Twitdata.read('twitdata.ini')
+
+print "Split this file into seperate files and split config into twitter config and parameter config"
+
+## authentication
+auth = tweepy.OAuthHandler(Twitdata.get('Tweepy','consumer_key'), Twitdata.get('Tweepy','consumer_secret'))
+auth.set_access_token(Twitdata.get('Tweepy','access_key'), Twitdata.get('Tweepy','access_secret'))
+
+## initialize API
+api = tweepy.API(auth)
+
+exitFlag = 0
+queueLock = threading.Lock()
+countsLock = threading.Lock()
+
+class Top10Streamer(StreamListener):
+
+	def __init__(self, q, api = None):
+		self.api = api or API()
+		self.output  = 'tweets/'
+		if not os.path.exists(os.path.dirname(os.path.abspath('__file__'))+"/tweets"):
+			os.makedirs(os.path.dirname(os.path.abspath('__file__'))+"/tweets")
+		self.delout  = open('tweets/delete.txt', 'a')
+		self.q = q
+
+	def on_status(self, status):
+		
+		# save tweet to file
+		if status.lang == "nl":
+			with open(self.output + status.created_at.strftime("%Y-%m-%d.%H.00.00") + ".json", "a") as doc:
+				doc.write(json.dumps(status._json) + "\n")
+
+			# put tweet in queue
+			queueLock.acquire()
+			self.q.put(status)
+			queueLock.release()
+		return 
+
+	def on_delete(self, status_id, user_id):
+		self.delout.write( str(status_id) + "\n")
+		return
+
+	def on_limit(self, track):
+		sys.stderr.write(track + "\n")
+		return
+
+	def on_error(self, status_code):
+		sys.stderr.write('Error: ' + str(status_code) + "\n")
+		return False
+
+	def on_timeout(self):
+		sys.stderr.write("Timeout, sleeping for 60 seconds...\n")
+		time.sleep(60)
+		return 
+
+class listenerThread(threading.Thread):
+	def __init__(self, threadID, name, q, keywords):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.q = q
+		self.keywords = keywords
+	def run(self):
+		print "Starting " + self.name
+		start_streaming(self.q,self.keywords)
+		print "Exiting " + self.name
+
+class workerThread(threading.Thread):
+	def __init__(self,threadID,name,q,counts):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.q = q
+		self.counts = counts
+	def run(self):
+		print "Starting " + self.name
+		process_tweet(self.q,self.counts,self.name)
+		print "Exiting " + self.name
+
+def process_tweet(tweetQueue,tCounts,name):
+	while not exitFlag:
+		queueLock.acquire()
+		if not tweetQueue.empty():
+			tweet = tweetQueue.get()
+			queueLock.release()
+			
+			# tokenize tweet.text
+			tokens = tokenizeRawTweetText(tweet.text)
+			
+			# update counts
+			countsLock.acquire()
+			tCounts.update(tokens,calendar.timegm(tweet.created_at.timetuple()))
+			interval = calendar.timegm(datetime.datetime.utcnow().timetuple())-calendar.timegm(tCounts.last_output.timetuple())
+			if interval > tCounts.output_interval:
+				tCounts.makeOutput()
+			countsLock.release()
+		else:
+			queueLock.release()
+
+def start_streaming(tweetQueue,keywords):
+	listen = Top10Streamer(tweetQueue,api)
+	stream = tweepy.Stream(auth, listen)
+	print "Streaming started at " + datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S +0000 %Y")
+	while True:
+		try:
+			stream.filter(track = keywords)
+		except:
+			print "Streaming disconnected at " + datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S +0000 %Y")
+			stream.disconnect()
+
+	print "Streaming stopped at " + datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S +0000 %Y")
+
+def union(keywords, tokens):
+    """ return the union of two lists """
+    union = []
+    for t in tokens:
+    	if t in keywords:
+    		union.append(t)
+    	elif t.strip('#') in keywords:
+    		union.append(t.strip('#'))
+    return union
+
+def main():
+	threadList = ["Worker-1", "Worker-2", "Worker-3"]
+	tweetQueue = Queue.Queue(100000)
+	threads = []
+	
+	with open("keywords_top10app.txt") as doc:
+		keywords = unicode(doc.read()).lower().split(',')
+
+	count = tokenCounts(keywords,Param.getint('Parameters','min_interval'))
+ 	
+	try:
+		# Create listener thread
+		thread = listenerThread(1,"Listener",tweetQueue,keywords)
+		thread.start()
+		threads.append(thread)
+
+		# Create worker threads
+		threadID = 2
+		for tName in threadList:
+			thread = workerThread(threadID, tName, tweetQueue, count)
+			thread.start()
+			threads.append(thread)
+			threadID += 1
+	except:
+		exitFlag = 1
+		for t in threads:
+			t.join()
+			print "Exiting Main Thread"
+
+
+if __name__ == '__main__': main()	
---
+++ tokencounts.py
@@ -0,0 +1,61 @@
+import os, datetime, calendar, json
+
+class tokenCounts:
+	def __init__(self,keywords,min_interval):
+		self.keywords = keywords
+		self.keywordCounts = {}
+		self.subCounts = {}
+		self.resultsdict = {}
+		self.last_output = datetime.datetime.utcnow()
+		self.output_interval = 60*min_interval # output interval in seconds
+		self.output = "results/"
+		if not os.path.exists(os.path.dirname(os.path.abspath('__file__'))+"/results"):
+			os.makedirs(os.path.dirname(os.path.abspath('__file__'))+"/results")
+
+	def update(self,tokens,timestamp):
+		self.resultsdict[timestamp] = tokens
+
+	def setInitialScores(self):
+		for word in self.keywords:
+			self.keywordCounts[word] = 0
+			self.subCounts[word] = {}
+
+	def calcScore(self):
+		self.setInitialScores()
+		for entry in self.resultsdict:
+			toUpdate = union(self.keywords,self.resultsdict[entry])
+			for word in toUpdate:
+				try:
+					self.keywordCounts[word] += 1
+					for t in self.resultsdict[entry]:
+						if t != word and t.strip('#') != word:
+							if t in self.subCounts[word]:
+								self.subCounts[word][t] += 1
+							else:
+								self.subCounts[word][t] = 1
+				except KeyError:
+					pass
+
+	def removeOldEntries(self):
+		to_delete = []
+		cur_ts = calendar.timegm(datetime.datetime.utcnow().timetuple())
+		for entry in self.resultsdict:
+			if cur_ts-entry > self.output_interval:
+				to_delete.append(entry)
+
+		temp_dict = {}
+		for key in self.resultsdict:
+			if key not in to_delete:
+				temp_dict[key] = self.resultsdict[key]
+		self.resultsdict = temp_dict
+
+	def makeOutput(self):
+		# output results to json file
+
+		self.calcScore()
+
+		self.last_output = datetime.datetime.utcnow()
+		with open(self.output + self.last_output.strftime("%Y-%m-%d.%H.%M.00") + ".json", "w") as doc:
+				doc.write(json.dumps({"counts": self.keywordCounts, "subcounts": self.subCounts}))
+
+		self.removeOldEntries()
