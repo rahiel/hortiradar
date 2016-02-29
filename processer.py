d9a13e9a534fbf39cbfd9f79a50b25d870a25a5d
Marijn ten Thij <mctenthij@gmail.com>
2016-02-29 14:20:52 +0100

    Rewritten code and added comments

--- processer.py
+++ processer.py
@@ -1,159 +1,144 @@
 #!/usr/bin/python
-from tokencounts import tokenCounts
-from tweepy import StreamListener
+from datetime import datetime
+from calendar import timegm
+from Queue import Queue
+from threading import Thread, Lock
+from ConfigParser import ConfigParser
+
+from tweepy import StreamListener, OAuthHandler, API, Stream
 from tweepy.parsers import RawParser
 from twokenize import tokenizeRawTweetText
-import json, time, tweepy, sys, datetime, calendar, os, Queue, threading, ConfigParser
 
-Param = ConfigParser.ConfigParser()
-Param.read('parameters.ini')
-Twitdata = ConfigParser.ConfigParser()
-Twitdata.read('twitdata.ini')
+from tokencounts import tokenCounts
+from streamer import Top10Streamer
 
-## authentication
-auth = tweepy.OAuthHandler(Twitdata.get('Tweepy','consumer_key'), Twitdata.get('Tweepy','consumer_secret'))
-auth.set_access_token(Twitdata.get('Tweepy','access_key'), Twitdata.get('Tweepy','access_secret'))
+class listenerThread(Thread):
+	"""Thread that is used to receive tweets through Twitter API"""
 
-## initialize API
-api = tweepy.API(auth)
+	def __init__(self, name, queue, conf, track):
+		"""
+		Parameters
+		name:	name of the thread
+		queue:	queue of tweets that need to be processed
+		conf:	location of config file for Twitter API authentication
+		track:	keywords that are used to filter twitter messages
+		"""
+		Thread.__init__(self)
+		self.name = name
+		self.q = queue
+		self.configLocation = conf
+		self.keywords = track
 
-global keywords
-with open("keywords_bloemen_top10app.txt") as doc:
-	keywords = unicode(doc.read()).lower().split(',')
+	def run(self):
+		"""Initiates the streaming process"""
+		print "Starting " + self.name
+		self.start_streaming()
+		print "Exiting " + self.name
 
-exitFlag = 0
-queueLock = threading.Lock()
-countsLock = threading.Lock()
-
-class Top10Streamer(StreamListener):
-
-	def __init__(self, q, api = None):
-		self.api = api or API()
-		self.output  = 'tweets/'
-		if not os.path.exists(os.path.dirname(os.path.abspath('__file__'))+"/tweets"):
-			os.makedirs(os.path.dirname(os.path.abspath('__file__'))+"/tweets")
-		self.delout  = open('tweets/delete.txt', 'a')
-		self.q = q
-
-	def on_status(self, status):
+	def start_streaming(self):
+		"""Initiates the streaming of tweets fetching all tweets that contain one of the given keywords"""
 		
-		# save tweet to file
-		if "lievelingsbloem" in status.text.lower():
-			with open(self.output + "lievelingsbloem/" + status.created_at.strftime("%Y-%m-%d.%H.00.00") + ".json", "a") as doc:
-				doc.write(json.dumps(status._json) + "\n")
-		# if "twuinbijt" in status.text.lower():
-			# with open(self.output + "twuinbijt/" + status.created_at.strftime("%Y-%m-%d.%H.00.00") + ".json", "a") as doc:
-				# doc.write(json.dumps(status._json) + "\n")
-		if "tulpendag" in status.text.lower():
-			with open(self.output + "tulpendag/" + status.created_at.strftime("%Y-%m-%d.%H.00.00") + ".json", "a") as doc:
-				doc.write(json.dumps(status._json) + "\n")
-		else:
-			with open(self.output + status.created_at.strftime("%Y-%m-%d.%H.00.00") + ".json", "a") as doc:
-				doc.write(json.dumps(status._json) + "\n")
+		## Load API config data
+		Twitdata = ConfigParser()
+		Twitdata.read(self.configLocation)
 
-			# put tweet in queue
-			queueLock.acquire()
-			self.q.put(status)
-			queueLock.release()
+		## initialize connection with Twitter API
+		auth = OAuthHandler(Twitdata.get('Tweepy','consumer_key'), Twitdata.get('Tweepy','consumer_secret'))
+		auth.set_access_token(Twitdata.get('Tweepy','access_key'), Twitdata.get('Tweepy','access_secret'))
+		api = API(auth)
+
+		## set up Streamer
+		listen = Top10Streamer(self.q,api)
+		stream = Stream(auth, listen)
+		print "Streaming started at " + datetime.utcnow().strftime("%a %b %d %H:%M:%S +0000 %Y")
+		while True:
+			try:
+				stream.filter(track = self.keywords)
+			except:
+				print "Streaming disconnected at " + datetime.utcnow().strftime("%a %b %d %H:%M:%S +0000 %Y")
+				stream.disconnect()
+
+		print "Streaming stopped at " + datetime.utcnow().strftime("%a %b %d %H:%M:%S +0000 %Y")
+
+class workerThread(Thread):
+	"""Thread that processes the tweets to count occurences of keywords"""
+	
+	def __init__(self,threadID,name):
+		"""
+		Parameters
+		threadID: 	id of thread
+		name: 		name of thread
+		"""
+		Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		
+	def run(self):
+		"""Initiates the processing"""
+		print "Starting " + self.name
+		self.process_tweets()
+		print "Exiting " + self.name
+
+	def process_tweets(self):
+		"""Handles the processing of tweets"""
+		while not __exitFlag:
+			__queueLock.acquire()
+			if not __tweetQueue.empty():
+				tweet = __tweetQueue.get()
+				__queueLock.release()
 				
-		return 
+				# tokenize tweet.text
+				tokens = tokenizeRawTweetText(tweet.text)
+				
+				# update counts
+				__countsLock.acquire()
+				__tCounts.update(tokens,timegm(tweet.created_at.timetuple()))
+				interval = timegm(datetime.utcnow().timetuple())-timegm(__tCounts.last_output.timetuple())
+				if interval > __tCounts.output_interval:
+					__tCounts.makeOutput()
+				__countsLock.release()
+			else:
+				__queueLock.release()
 
-	def on_delete(self, status_id, user_id):
-		self.delout.write( str(status_id) + "\n")
-		return
-
-	def on_limit(self, track):
-		sys.stderr.write(track + "\n")
-		return
-
-	def on_error(self, status_code):
-		sys.stderr.write('Error: ' + str(status_code) + "\n")
-		return False
-
-	def on_timeout(self):
-		sys.stderr.write("Timeout, sleeping for 60 seconds...\n")
-		time.sleep(60)
-		return 
-
-class listenerThread(threading.Thread):
-	def __init__(self, threadID, name, q, keywords):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.name = name
-		self.q = q
-		self.keywords = keywords
-	def run(self):
-		print "Starting " + self.name
-		start_streaming(self.q,self.keywords)
-		print "Exiting " + self.name
-
-class workerThread(threading.Thread):
-	def __init__(self,threadID,name,q,counts):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.name = name
-		self.q = q
-		self.counts = counts
-	def run(self):
-		print "Starting " + self.name
-		process_tweet(self.q,self.counts,self.name)
-		print "Exiting " + self.name
-
-def process_tweet(tweetQueue,tCounts,name):
-	while not exitFlag:
-		queueLock.acquire()
-		if not tweetQueue.empty():
-			tweet = tweetQueue.get()
-			queueLock.release()
-			
-			# tokenize tweet.text
-			tokens = tokenizeRawTweetText(tweet.text)
-			
-			# update counts
-			countsLock.acquire()
-			tCounts.update(tokens,calendar.timegm(tweet.created_at.timetuple()))
-			interval = calendar.timegm(datetime.datetime.utcnow().timetuple())-calendar.timegm(tCounts.last_output.timetuple())
-			if interval > tCounts.output_interval:
-				tCounts.makeOutput()
-			countsLock.release()
-		else:
-			queueLock.release()
-
-def start_streaming(tweetQueue,keywords):
-	listen = Top10Streamer(tweetQueue,api)
-	stream = tweepy.Stream(auth, listen)
-	print "Streaming started at " + datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S +0000 %Y")
-	while True:
-		try:
-			stream.filter(track = keywords)
-		except:
-			print "Streaming disconnected at " + datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S +0000 %Y")
-			stream.disconnect()
-
-	print "Streaming stopped at " + datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S +0000 %Y")
 
 def main():
+	"""Starts streaming and processing threads and initiates tokencounter"""
 	threadList = ["Worker-1", "Worker-2", "Worker-3"]
-	tweetQueue = Queue.Queue(100000)
 	threads = []
 
-	count = tokenCounts(keywords,Param.getint('Parameters','min_interval'))
+	Param = ConfigParser()
+	Param.read('data/parameters.ini')
  	
+ 	global __exitFlag
+	global __tweetQueue
+	global __queueLock
+	global __countsLock
+	global __tCounts
+
+	__exitFlag = 0
+	__tweetQueue = Queue(100000)
+	__queueLock = Lock()
+	__countsLock = Lock()
+	__tCounts = tokenCounts(keywords,Param.getint('Parameters','min_interval'))
+
 	try:
+		with open("data/keywords_groentefruit_top10app.txt") as doc:
+			keywords = unicode(doc.read()).lower().split(',')
+
 		# Create listener thread
-		thread = listenerThread(1,"Listener",tweetQueue,keywords)
+		thread = listenerThread("Listener",queue=__tweetQueue,conf="data/twitdata.ini",track=keywords)
 		thread.start()
 		threads.append(thread)
 
 		# Create worker threads
 		threadID = 2
 		for tName in threadList:
-			thread = workerThread(threadID, tName, tweetQueue, count)
+			thread = workerThread(threadID, tName)
 			thread.start()
 			threads.append(thread)
 			threadID += 1
 	except:
-		exitFlag = 1
+		__exitFlag = 1
 		for t in threads:
 			t.join()
 			print "Exiting Main Thread"
---
+++ streamer.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+from json import dumps
+from time import sleep
+from sys import stderr
+from os import makedirs
+from os.path import exists, abspath, dirname
+from tweepy import StreamListener, API
+
+from tokencounts import tokenCounts
+
+class Top10Streamer(StreamListener):
+
+	def __init__(self, q, api = None):
+		"""
+		Object that handles incoming data of the twitter streaming API
+
+		Parameters
+		q: 		queue of tweets that need to be processed
+		output: folder where tweets are saved 									(TBD: auto-store tweets in DB)
+		delout: file where ID's of tweets that have to be deleted are stored 	(TBD: auto-delete in DB)
+		api: 	twitter API-object
+		"""
+		self.api = api or API()
+		self.output  = 'tweets/'
+		if not exists(dirname(abspath('__file__'))+"/tweets"):
+			makedirs(dirname(abspath('__file__'))+"/tweets")
+		self.delout  = open('tweets/delete.txt', 'a')
+		self.q = q
+
+	def on_status(self, status):
+		"""
+		Called when a new status arrives
+
+		Parameters
+		status: 	status object received from Twitter
+		"""
+
+		with open(self.output + status.created_at.strftime("%Y-%m-%d.%H.00.00") + ".json", "a") as doc:
+			doc.write(dumps(status._json) + "\n")
+
+		__queueLock.acquire()
+		self.q.put(status)
+		__queueLock.release()		
+		return 
+
+	def on_delete(self, status_id, user_id):
+		"""
+		Called when a delete notice arrives for a status
+
+		Parameters
+		status_id: 	id of the status that has to be removed
+		user_id:	id of the user that removed the status
+		"""
+		self.delout.write( str(status_id) + "\n")
+		return
+
+	def on_limit(self, track):
+		"""
+		Called when a limitation notice arrives
+
+		Parameters
+		track:		dictionary with information regarding limitation
+		"""
+		stderr.write(track + "\n")
+		return
+
+	def on_error(self, status_code):
+		"""Called when a non-200 status code is returned"""
+		stderr.write('Error: ' + str(status_code) + "\n")
+		return False
+
+	def on_timeout(self):
+		"""Called when stream connection times out"""
+		stderr.write("Timeout, sleeping for 60 seconds...\n")
+		sleep(60)
+		return 
+
+	def on_disconnect(self, notice):
+		"""Called when twitter sends a disconnect notice
+		Disconnect codes are listed here:
+		https://dev.twitter.com/docs/streaming-apis/messages#Disconnect_messages_disconnect
+		"""
+		# TBD: notify if disconnected
+		return 
