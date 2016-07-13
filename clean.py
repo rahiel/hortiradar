167872f1d7d3adaccd1300e27ffba514f3980b94
Rahiel Kasim <rahielkasim@gmail.com>
2016-07-13 17:25:17 +0200

    delete tweets older than 2 weeks and with 0 keywords

--- clean.py
+++ clean.py
@@ -1,5 +1,22 @@
+from datetime import datetime, timedelta
+
 from main import get_db
 
 
-db = get_db()
-db.tweets.delete_many({"num_keywords": 0})
+tweets = get_db().tweets
+
+# tweet timestamps are in UTC time
+two_weeks_ago = datetime.utcnow() - timedelta(days=14)
+
+# parse tweet timestamps
+tw = tweets.find({"datetime": {"$exists": False}})
+time_format = "%a %b %d %H:%M:%S +0000 %Y"  # 'Tue Jun 28 15:01:54 +0000 2016'
+for t in tw:
+    dt = datetime.strptime(t["tweet"]["created_at"], time_format)
+    tweets.update_one({"_id": t["_id"]}, {"$set": {"datetime": dt}})
+
+
+tweets.delete_many({
+    "num_keywords": 0,
+    "datetime": {"$lt": two_weeks_ago}
+})
