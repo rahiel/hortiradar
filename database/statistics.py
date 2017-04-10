535f14c5f0542938b8b95348c1c9adebc7a19687
Rahiel Kasim <rahielkasim@gmail.com>
2017-04-10 15:53:32 +0200

    website: use , as thousands separator for number of tweets

--- database/statistics.py
+++ database/statistics.py
@@ -7,6 +7,9 @@ from keywords import get_db
 db = get_db()
 redis = StrictRedis()
 
+format_count = lambda c: "{:,}".format(c)  # use , for thousands separator
+clean_count = lambda c: int(c.replace(",", ""))
+
 stats = db.command("collstats", "tweets", scale=int(1E9))
 count = stats["count"]               # number of tweets
 size = stats["size"]                 # raw collection size
@@ -19,14 +22,14 @@ latest_date = latest_tweet["datetime"].strftime("%Y-%m-%d")
 
 try:
     prev_stats = json.loads(redis.get("t:stats"))
-    prev_count = prev_stats["count"]
+    prev_count = clean_count(prev_stats["count"])
 except TypeError:
     prev_count = 0
 count_24h = count - prev_count  # number of tweets in the last 24h
 
 stats = {
-    "count": count,
-    "count_24h": count_24h,
+    "count": format_count(count),
+    "count_24h": format_count(count_24h),
     "size": size,
     "storage_size": storage_size,
     "date_oldest_tweet": oldest_date,
