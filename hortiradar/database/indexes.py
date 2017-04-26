3156d4076c052562b266c37e1d8d426f5875fea5
Rahiel Kasim <rahielkasim@gmail.com>
2017-04-26 18:15:14 +0200

    database: fix index

--- hortiradar/database/indexes.py
+++ hortiradar/database/indexes.py
@@ -7,4 +7,4 @@ tweets = get_db().tweets
 tweets.create_index([("num_keywords", 1), ("datetime", 1)])  # api:/keywords, statistics.py
 tweets.create_index([("groups", 1), ("datetime", 1)])        # api:/keywords, api:/groups/{group}
 tweets.create_index([("keywords", 1), ("datetime", 1)])      # api:/keywords/{keyword}/*
-tweets.create_index([("id_str", 1)])                         # api:/tweet/{id_str}
+tweets.create_index("tweet.id_str")                          # api:/tweet/{id_str}
