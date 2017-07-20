0b7b787b9c8ccdf9569021f257dd1b928a1644f0
Rahiel Kasim <rahielkasim@gmail.com>
2017-07-20 12:59:07 +0200

    website: user roles

--- hortiradar/website/models.py
+++ hortiradar/website/models.py
@@ -18,3 +18,17 @@ class User(db.Model, UserMixin):
     active = db.Column("is_active", db.Boolean(), nullable=False, server_default="0")
     first_name = db.Column(db.String(100), nullable=False, server_default="")
     last_name = db.Column(db.String(100), nullable=False, server_default="")
+
+    roles = db.relationship("Role", secondary="user_roles",
+                            backref=db.backref("users", lazy="dynamic"))
+
+
+class Role(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+
+class UserRoles(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id", ondelete="CASCADE"))
+    role_id = db.Column(db.Integer(), db.ForeignKey("role.id", ondelete="CASCADE"))
