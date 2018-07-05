8417a8a2d6e423aa967bafc29e7f50e9292802e8
Marijn ten Thij <mctenthij@gmail.com>
2018-07-05 15:59:04 +0200

    clustering: typo fix in run_storify and PEP8 fixes

--- hortiradar/clustering/storify.py
+++ hortiradar/clustering/storify.py
@@ -7,9 +7,8 @@ import gensim
 import numpy as np
 from redis import StrictRedis
 from scipy.sparse import csgraph
-import ujson as json
 
-from hortiradar.clustering import Config, ExtendedTweet, Cluster, Stories, tweet_time_format
+from hortiradar.clustering import Config, ExtendedTweet, Cluster, Stories
 from hortiradar.clustering.util import round_time
 from hortiradar.database import get_db, get_keywords
 
@@ -23,8 +22,8 @@ keywords = get_keywords(local=True)
 
 redis = StrictRedis()
 
-spam_level = Config.getfloat('database:parameters',"spam_level")
-tweet_threshold = Config.getfloat('storify:parameters','tweet_threshold')
+spam_level = Config.getfloat('database:parameters', "spam_level")
+tweet_threshold = Config.getfloat('storify:parameters', 'tweet_threshold')
 
 def is_spam(t):
     return t.get("spam") is not None and t["spam"] > spam_level
@@ -32,11 +31,11 @@ def is_spam(t):
 def get_filt_tokens(tweet):
     return [t.lemma for t in tweet.tokens if not t.filter_token()]
 
-def get_tweets(start,end,key,keytype):
+def get_tweets(start, end, key, keytype):
     jsontweets = tweetsdb.find({
         keytype: key,
         "datetime": {"$gte": start, "$lt": end}
-    },projection={
+    }, projection={
         "tweet.id_str": True, "tokens": True, "tweet.entities": True, "tweet.created_at": True,
         "tweet.user.id_str": True, "tweet.user.screen_name": True, "tweet.retweeted_status.user.id_str": True,
         "tweet.retweeted_status.user.screen_name": True, "tweet.retweeted_status.id_str": True,
@@ -51,11 +50,10 @@ def get_tweets(start,end,key,keytype):
 
     return tweets
 
-def perform_clustering(tweets,stories,key):
-    retweets = []
+def perform_clustering(tweets, stories, key):
     non_retweets = []
     for tw in tweets:
-        if hasattr(tw.tweet,"retweeted_status"):
+        if hasattr(tw.tweet, "retweeted_status"):
             rt_id_str = tw.tweet.retweeted_status.id_str
             for story in stories:
                 story_tweet_ids = [tw.tweet.id_str for tw in story.tweets]
@@ -66,42 +64,42 @@ def perform_clustering(tweets,stories,key):
             non_retweets.append(tw)
 
     if non_retweets:
-        clusters = perform_clustering_tweets(non_retweets,key)
+        clusters = perform_clustering_tweets(non_retweets, key)
     else:
         clusters = []
 
     return clusters, stories
 
-def perform_clustering_tweets(tweets,key):
+def perform_clustering_tweets(tweets, key):
     texts = [get_filt_tokens(tw) for tw in tweets]
     dictionaries = gensim.corpora.Dictionary(texts)
     corpus = [dictionaries.doc2bow(text) for text in texts]
-    sims = gensim.similarities.Similarity('/tmp/clustering_{g}'.format(g=key),corpus,num_features=len(dictionaries))
+    sims = gensim.similarities.Similarity('/tmp/clustering_{g}'.format(g=key), corpus, num_features=len(dictionaries))
 
     mat = []
-    for j,tw in enumerate(tweets):
+    for j, tw in enumerate(tweets):
         mat.append([1 if x > tweet_threshold else 0 for x in sims[dictionaries.doc2bow(get_filt_tokens(tw))]])
 
-    n_clusters, cluster_labels = csgraph.connected_components(mat,directed=False)
+    n_clusters, cluster_labels = csgraph.connected_components(mat, directed=False)
     clusters = [Cluster() for _ in range(n_clusters)]
 
-    for num,label in enumerate(cluster_labels):
+    for num, label in enumerate(cluster_labels):
         clusters[label].add_tweet(tweets[num])
 
     return clusters
 
-def storify_clusters(stories,clusters):
-    if not stories:        
+def storify_clusters(stories, clusters):
+    if not stories:
         for c in clusters:
             stories.append(Stories(c))
     else:
         matched_boolean = [False]*len(stories)
 
         for c in clusters:
-            sim,vals = zip(*[s.is_similar(c) for s in stories])
+            sim, vals = zip(*[s.is_similar(c) for s in stories])
             match = False
             for m in np.flipud(np.argsort(vals)):
-                if sim[m] == True:
+                if sim[m]:
                     match = True
                     break
 
@@ -112,8 +110,8 @@ def storify_clusters(stories,clusters):
                 stories.append(Stories(c))
                 matched_boolean.append(True)
 
-        for j,story in enumerate(stories):
-            if matched_boolean[j] == False:
+        for j, story in enumerate(stories):
+            if not matched_boolean[j]:
                 story.add_delay()
                 if story.close_story():
                     story.end_story()
@@ -130,9 +128,9 @@ def find_finished_stories(stories):
         else:
             active_stories.append(story)
 
-    return active_stories,finished_stories
+    return active_stories, finished_stories
 
-def output_stories(stories,key,keytype):
+def output_stories(stories, key, keytype):
     for story in stories:
         storydict = story.get_jsondict()
 
@@ -141,23 +139,23 @@ def output_stories(stories,key,keytype):
 
         storiesdb.insert_one(storydict)
 
-def run_storify(stories,key,keytpe):
+def run_storify(stories, key, keytype):
     end = round_time(datetime.utcnow())
     start = end - timedelta(hours=1)
-    tweets = get_tweets(start,end,key,keytype)
-    
-    if tweets:
-        clusters, stories = perform_clustering(tweets,stories,key)
+    tweets = get_tweets(start, end, key, keytype)
 
-        stories = storify_clusters(stories,clusters)
-        stories,finished_stories = find_finished_stories(stories)
-        
+    if tweets:
+        clusters, stories = perform_clustering(tweets, stories, key)
+
+        stories = storify_clusters(stories, clusters)
+        stories, finished_stories = find_finished_stories(stories)
+
         if finished_stories:
-            output_stories(finished_stories,key,keytype)
+            output_stories(finished_stories, key, keytype)
 
     return stories
 
-def process_key(key,keytype):
+def process_key(key, keytype):
     k = "s:{k}".format(k=key)
     v = redis.get(k)
     if v:
@@ -165,14 +163,14 @@ def process_key(key,keytype):
     else:
         stories = []
 
-    stories = run_storify(stories,key,keytype)
+    stories = run_storify(stories, key, keytype)
 
     stories_out = pickle.dumps(stories)
-    redis.set(k,stories_out,ex=60*90)
+    redis.set(k, stories_out, ex=60*90)
 
 def main(argv):
     try:
-        opts, args = getopt(argv,"ht:",["type="])
+        opts, args = getopt(argv, "ht:", ["type="])
     except GetoptError:
         print ('test.py -t <processing type>')
         sys.exit(2)
@@ -180,13 +178,13 @@ def main(argv):
         if opt == "-h":
             print ('test.py -t <processing type>')
             sys.exit()
-        elif opt in ["-t","--type"]:
+        elif opt in ["-t", "--type"]:
             if arg == "groups":
                 for group in groups:
-                    process_key(group,arg)
+                    process_key(group, arg)
             elif arg == "keywords":
                 for keyword in keywords:
-                    process_key(keyword,arg)
+                    process_key(keyword, arg)
             else:
                 raise(NotImplementedError)
 
