438ff77edcd76e7a18902d11abe389d2493910ba
Rahiel Kasim <rahielkasim@gmail.com>
2018-12-06 16:15:05 +0100

    every week clean untagged tweets older than 15 months

--- hortiradar/database/clean.py
+++ hortiradar/database/clean.py
@@ -6,9 +6,9 @@ from keywords import get_db
 tweets = get_db().tweets
 
 # tweet timestamps are in UTC time
-two_weeks_ago = datetime.utcnow() - timedelta(days=14)
+limit = datetime.utcnow() - timedelta(days=31 * 15)
 
 tweets.delete_many({
     "num_keywords": 0,
-    "datetime": {"$lt": two_weeks_ago}
+    "datetime": {"$lt": limit}
 })
